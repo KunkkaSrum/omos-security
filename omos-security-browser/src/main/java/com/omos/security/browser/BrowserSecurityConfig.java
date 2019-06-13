ea724c506e264a02f03c361a07601d0e1c43c35f
hyh <huangyh9511@163.com>
2019-06-13 22:10:15 +0800

    commit

--- omos-security-browser/src/main/java/com/omos/security/browser/BrowserSecurityConfig.java
+++ omos-security-browser/src/main/java/com/omos/security/browser/BrowserSecurityConfig.java
@@ -15,8 +15,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.session.InvalidSessionStrategy;
+import org.springframework.security.web.session.SessionInformationExpiredStrategy;
 import org.springframework.social.security.SpringSocialConfigurer;
 
 import javax.sql.DataSource;
@@ -38,46 +42,47 @@ public class BrowserSecurityConfig extends AbstractChannelSecurityConfig {
 
     @Autowired
     private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
-    //
+
     @Autowired
     private ValidateCodeSecurityConfig validateCodeSecurityConfig;
 
     @Autowired
     private SpringSocialConfigurer imoocSocialSecurityConfig;
-//
-//	@Autowired
-//	private SessionInformationExpiredStrategy sessionInformationExpiredStrategy;
-//
-//	@Autowired
-//	private InvalidSessionStrategy invalidSessionStrategy;
 
-    //	@Autowired
-//	protected AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
-//
-//	@Autowired
-//	protected AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+    @Autowired
+    private SessionInformationExpiredStrategy sessionInformationExpiredStrategy;
+
+    @Autowired
+    private InvalidSessionStrategy invalidSessionStrategy;
+
+    @Autowired
+    protected AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
+
+    @Autowired
+    protected AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         applyPasswordAuthenticationConfig(http);
 
         http
                 .apply(validateCodeSecurityConfig)
-                    .and()
+                .and()
                 .apply(smsCodeAuthenticationSecurityConfig)
-                    .and()
+                .and()
                 .apply(imoocSocialSecurityConfig)
-                    .and()
+                .and()
                 .rememberMe()
-                    .tokenRepository(persistentTokenRepository())
-                    .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
-                    .userDetailsService(userDetailsService)
-//				.and()
-//				.sessionManagement()
-//				.invalidSessionStrategy(invalidSessionStrategy)
-//				.maximumSessions(securityProperties.getBrowser().getSession().getMaximumSessions())
-//				.maxSessionsPreventsLogin(securityProperties.getBrowser().getSession().isMaxSessionsPreventsLogin())
-//				.expiredSessionStrategy(sessionInformationExpiredStrategy)
-//				.and()
+                .tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+                .userDetailsService(userDetailsService)
+                .and()
+                .sessionManagement()
+                .invalidSessionStrategy(invalidSessionStrategy)
+                .maximumSessions(securityProperties.getBrowser().getSession().getMaximumSessions())
+                .maxSessionsPreventsLogin(securityProperties.getBrowser().getSession().isMaxSessionsPreventsLogin())
+                .expiredSessionStrategy(sessionInformationExpiredStrategy)
+                .and()
                 .and()
                 .authorizeRequests()
                 .antMatchers(
